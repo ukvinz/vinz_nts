ee45eb15dfd9b445279dfdced05f26b831f87c0f
Vince <120654757+ukvinz@users.noreply.github.com>
2024-12-30 12:07:51 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -6,14 +6,14 @@ const CONFIG = {
   NEXT_NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
   NEXT_POST_LIST_COVER: false, // 文章列表显示封面图
-  NEXT_POST_LIST_PREVIEW: true, // 显示文章预览
+  NEXT_POST_LIST_PREVIEW: false, // 显示文章预览
   NEXT_POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
   NEXT_RIGHT_BAR: true, // 是否显示右侧栏
-  NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
+  NEXT_RIGHT_LATEST_POSTS: false, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
   NEXT_RIGHT_AD: false, // 右侧广告
@@ -31,7 +31,7 @@ const CONFIG = {
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
   NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
-  NEXT_ARTICLE_COPYRIGHT: true, // 文章版权声明
+  NEXT_ARTICLE_COPYRIGHT: false, // 文章版权声明
   NEXT_ARTICLE_INFO: true // 显示文章信息
 
 }
