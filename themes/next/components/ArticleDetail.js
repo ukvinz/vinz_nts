048a0f5baf608b5b620ce14deb6c8272d90f4f1b
Vince <120654757+ukvinz@users.noreply.github.com>
2025-01-07 09:03:41 +0800

    Update ArticleDetail.js

--- themes/next/components/ArticleDetail.js
+++ themes/next/components/ArticleDetail.js
@@ -47,7 +47,7 @@ export default function ArticleDetail(props) {
               post?.type &&
               !post?.type !== 'Page' &&
               post?.pageCover && (
-                <div>
+                <div><br /><br />
                 </div>
               )}
 
