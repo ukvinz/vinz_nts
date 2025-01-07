7da7d898958b766ca46aeea7372df588f8a4b380
Vince <120654757+ukvinz@users.noreply.github.com>
2025-01-07 08:26:22 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -42,13 +42,7 @@ const Footer = ({ title }) => {
         <br />
         <h1>{title}</h1>
         <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>
-          Powered by{' '}
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className='underline '>
-            NotionNext {siteConfig('VERSION')}
-          </a>
-          .
+          All Rights Reserved
         </span>
       </span>
     </footer>
